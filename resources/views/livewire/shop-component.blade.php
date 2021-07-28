aaf6534251e502f76544d12685a6429d43b90f18
GautamSinghh <anupmishra.ajit@gmail.com>
2021-07-29 04:00:20 +0530

    Make Product Details Page Dynamic

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -5,7 +5,7 @@
 
             <div class="wrap-breadcrumb">
                 <ul>
-                    <li class="item-link"><a href="#" class="link">home</a></li>
+                    <li class="item-link"><a href="/" class="link">home</a></li>
                     <li class="item-link"><span>Digital & Electronics</span></li>
                 </ul>
             </div>
@@ -66,19 +66,19 @@
                             <li class="col-lg-4 col-md-6 col-sm-6 col-xs-6 ">
                                 <div class="product product-style-3 equal-elem ">
                                     <div class="product-thumnail">
-                                        <a href="detail.html" title="{{$product->name}}">
+                                        <a href="{{route('product.details',['slug'=>$product->slug])}}" title="{{$product->name}}">
                                             <figure><img src="{{ asset('assets/images/products') }}/{{$product->image}}" alt="{{$product->name}}"></figure>
                                         </a>
                                     </div>
                                     <div class="product-info">
-                                        <a href="#" class="product-name"><span>{{$product->name}}</span></a>
+                                        <a href="{{route('product.details',['slug'=>$product->slug])}}" class="product-name"><span>{{$product->name}}</span></a>
                                         <div class="wrap-price"><span class="product-price">{{$product->regular_price}}</span></div>
                                         <a href="#" class="btn add-to-cart">Add To Cart</a>
                                     </div>
                                 </div>
                             </li>
                             @endforeach
-    
+
 
                         </ul>
 
@@ -86,13 +86,7 @@
 
                     <div class="wrap-pagination-info">
                         {{$products->links()}}
-                        <!-- <ul class="page-numbers">
-                            <li><span class="page-number-item current">1</span></li>
-                            <li><a class="page-number-item" href="#">2</a></li>
-                            <li><a class="page-number-item" href="#">3</a></li>
-                            <li><a class="page-number-item next-link" href="#">Next</a></li>
-                        </ul>
-                        <p class="result-count">Showing 1-8 of 12 result</p> -->
+
                     </div>
                 </div>
                 <!--end main products area-->
