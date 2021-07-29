83baf90f59675bbd73bdf4778731e71d7137da6d
GautamSinghh <anupmishra.ajit@gmail.com>
2021-07-29 20:31:58 +0530

    Search Products By Category

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\DetailsComponent;
+use App\Http\Livewire\SearchComponent;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
@@ -37,6 +38,8 @@ Route::get('/checkout', CheckoutComponent::class);
 Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
 
 Route::get('/product-category/{category_slug}', CategoryComponent::class)->name('product.category');
+
+Route::get('/search', SearchComponent::class)->name('product.search');
 
 // Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 //     return view('dashboard');
